9771bd211f11a679cf521df5f30502b9f5c62c92
ShellyEhud <114569813+ShellyEhud@users.noreply.github.com>
2024-12-08 10:43:53 +0200

    Create AppointmentTimeSelection.js
    Generate a page that will show a set of available appointment times.

---
+++ frontend/src/features/Appointment/AppointmentTimeSelection.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { Button, Grid, Typography, Stack } from '@mui/material';
+
+const AppointmentTimeSelection = ({ onTimeSelected }) => {
+  const availableTimes = [
+    '6:30', '7:15', '8:00', '8:45', '9:30', '10:15', '11:00', '11:45', '18:00', '18:45', '19:30', '20:00'
+  ];
+
+  return (
+    <Stack alignItems={'center'} justifyContent={'center'} spacing={3}>
+      <Typography variant='h4' textAlign='center'>בחר את השעה</Typography>
+      <Typography variant='h6' textAlign='center' color='gray'>xx/xx/xxxx (יום)</Typography>
+      <Grid container spacing={2} justifyContent={'center'}>
+        {availableTimes.map((time) => (
+          <Grid item key={time}>
+            <Button variant='outlined' onClick={() => onTimeSelected(time)}>{time}</Button>
+          </Grid>
+        ))}
+      </Grid>
+    </Stack>
+  );
+};
+
+export default AppointmentTimeSelection;
