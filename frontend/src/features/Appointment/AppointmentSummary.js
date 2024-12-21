80f48628cda700af9918f6986f7bbb0a7e2f247c
YuvalBareket <yuval44226690@gmail.com>
2024-12-21 18:56:56 +0200

    fix scrolling

--- frontend/src/features/Appointment/AppointmentSummary.js
+++ frontend/src/features/Appointment/AppointmentSummary.js
@@ -1,12 +1,38 @@
 import React, { useState } from 'react';
 import { TextField, Button, Stack, Typography } from '@mui/material';
 
-const AppointmentSummary = ({ selectedOption ,selectedDate ,selectedTime, clientName, clientMail, clientPhone, specialRequest, onSpecialRequestChange, onClientNameChange, onClientMailChange, onClientPhoneChange, onSubmit }) => {
-  
+const AppointmentSummary = ({
+  selectedOption,
+  selectedDate,
+  selectedTime,
+  clientName,
+  clientMail,
+  clientPhone,
+  specialRequest,
+  onSpecialRequestChange,
+  onClientNameChange,
+  onClientMailChange,
+  onClientPhoneChange,
+  onSubmit,
+}) => {
   return (
-    <Stack alignItems={'center'} justifyContent={'center'} spacing={3}>
-      <Typography variant='h4' textAlign='center'>סיכום פגישה</Typography>
-      <Typography variant='h6' textAlign='center' color='gray'>{selectedOption}</Typography>
+    <Stack
+      alignItems={'center'}
+      justifyContent={'center'}
+      spacing={3}
+      sx={{
+        overflowY: 'auto',
+        height: '70vh',
+        width: '100%',
+      }}
+      p={4}
+    >
+      <Typography variant='h4' textAlign='center'>
+        סיכום פגישה
+      </Typography>
+      <Typography variant='h6' textAlign='center' color='gray'>
+        {selectedOption}
+      </Typography>
       <Typography variant='body2'>תאריך: {selectedDate}</Typography>
       <Typography variant='body1'>שעה: {selectedTime}</Typography>
       <TextField
@@ -35,14 +61,16 @@ const AppointmentSummary = ({ selectedOption ,selectedDate ,selectedTime, client
         onChange={(e) => onClientPhoneChange(e.target.value)}
       />
       <TextField
-        label="בקשה מיוחדת"
+        label='בקשה מיוחדת'
         multiline
         rows={4}
         value={specialRequest}
         onChange={(e) => onSpecialRequestChange(e.target.value)}
         fullWidth
       />
-      <Button variant='contained' onClick={onSubmit}>אישור</Button>
+      <Button variant='contained' onClick={onSubmit}>
+        אישור
+      </Button>
     </Stack>
   );
 };
